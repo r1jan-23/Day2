9ba9d3186025770778a2de204f58caca6c23e816
r1jan-23 <poudelrijan6@gmail.com>
2025-12-27 10:34:31 +0545

    Bubble Sort

---
+++ Day7_1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, temp;
+    int arr[100];
+
+    // Input size
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    // Input array elements
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Bubble Sort logic
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    // Output sorted array
+    printf("Sorted array:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
