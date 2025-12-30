9b5af379d3e11273f0174412d6c628862a6d5f74
r1jan-23 <poudelrijan6@gmail.com>
2025-12-30 12:35:08 +0545

    Avg

---
+++ Day9_2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+int n1,n2,n3;
+    printf("Enter 1st Number");
+scanf("%d",&n1);
+ printf("Enter 2nd Number");
+scanf("%d",&n2);
+ printf("Enter 3rd Number");
+scanf("%d",&n3);
+float a;
+a = (n1+n2+n3)/3;
+printf("Ans: %f",a);
+}
